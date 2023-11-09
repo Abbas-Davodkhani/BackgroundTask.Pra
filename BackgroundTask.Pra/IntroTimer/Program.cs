e64fb7ed68bf0975bac54d36ae5abb2a9e104f7c
Abbas-Davodkhani <107187833+Abbas-Davodkhani@users.noreply.github.com>
2023-11-09 12:43:57 +0330

    TImer

---
+++ BackgroundTask.Pra/IntroTimer/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace IntroTimer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Timer timer = new Timer(Program.WriteOnlinUsersCount);
+
+            Console.ReadKey();  
+        }
+
+        public static void WriteOnlinUsersCount(object stateInfo)
+        {
+            Random random = new Random();
+            Console.WriteLine(random.Next(50, 80));
+        }
+    }
+
+    public class OnlineUser
+    {
+        
+    }
+}
