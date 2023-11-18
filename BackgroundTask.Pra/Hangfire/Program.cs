323e71c698b866e1c6db37774a1e38556bfce535
Abbas-Davodkhani <107187833+Abbas-Davodkhani@users.noreply.github.com>
2023-11-18 17:44:11 +0330

    hangfire

---
+++ BackgroundTask.Pra/Hangfire/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Hangfire
+{
+    internal class Program
+    {
+ 
+        static void Main(string[] args)
+        {
+            using (var sw = new BackgroundJobServer())
+            {
+                var fire = BackgroundJob.Enqueue(
+                () => Console.WriteLine("\"Fire-and-forget!\""));
+            }
+            
+
+            Console.ReadKey();                          
+        }
+    }
+}
